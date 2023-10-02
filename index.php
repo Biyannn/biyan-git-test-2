a7c25696752a52325b8f843bceb1cb040b601796
Biyan <ayyubian07@gmail.com>
2023-10-02 10:21:39 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1> Hello World </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt cum delectus eum veniam fugiat officia aut in dolorum neque unde.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
